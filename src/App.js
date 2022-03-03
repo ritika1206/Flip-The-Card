1e98f5396c4635bb6e4fff2bba1b5caa64141300
ritika1206 <67223899+ritika1206@users.noreply.github.com>
2022-03-03 14:30:28 +0530

    valid card on continue red background issue fixed

--- src/App.js
+++ src/App.js
@@ -36,69 +36,136 @@ function App() {
     // console.log("yo");
     // console.log(validCard);
     // console.log(seq);
-    if(seq === "1"){
-      gameState === 0 && setValidCard(6);
-      gameState === 1 && setValidCard(3);
-      gameState === 2 && setValidCard(4);
-      gameState === 3 && setValidCard(1);
-      gameState === 4 && setValidCard(5);
-    }
-    if(seq === "2"){
-      gameState === 0 && setValidCard(8);
-      gameState === 1 && setValidCard(5);
-      gameState === 2 && setValidCard(6);
-      gameState === 3 && setValidCard(4);
-      gameState === 4 && setValidCard(6);
-    }
-    if(seq === "3"){
-      gameState === 0 && setValidCard(5);
-      gameState === 1 && setValidCard(7);
-      gameState === 2 && setValidCard(2);
-      gameState === 3 && setValidCard(1);
-      gameState === 4 && setValidCard(8);
-    }
-    if(seq === "4"){
-      gameState === 0 && setValidCard(9);
-      gameState === 1 && setValidCard(4);
-      gameState === 2 && setValidCard(1);
-      gameState === 3 && setValidCard(3);
-      gameState === 4 && setValidCard(7);
-    }
-    if(seq === "5"){
-      gameState === 0 && setValidCard(3);
-      gameState === 1 && setValidCard(2);
-      gameState === 2 && setValidCard(6);
-      gameState === 3 && setValidCard(9);
-      gameState === 4 && setValidCard(1);
-    }
-    if(seq === "6"){
-      gameState === 0 && setValidCard(4);
-      gameState === 1 && setValidCard(9);
-      gameState === 2 && setValidCard(7);
-      gameState === 3 && setValidCard(8);
-      gameState === 4 && setValidCard(3);
-    }
-    if(seq === "7"){
-      gameState === 0 && setValidCard(5);
-      gameState === 1 && setValidCard(9);
-      gameState === 2 && setValidCard(7);
-      gameState === 3 && setValidCard(4);
-      gameState === 4 && setValidCard(8);
-    }
-    if(seq === "8"){
-      gameState === 0 && setValidCard(1);
-      gameState === 1 && setValidCard(2);
-      gameState === 2 && setValidCard(5);
-      gameState === 3 && setValidCard(9);
-      gameState === 4 && setValidCard(4);
-    }
-    if(seq === "9"){
-      gameState === 0 && setValidCard(6);
-      gameState === 1 && setValidCard(8);
-      gameState === 2 && setValidCard(7);
-      gameState === 3 && setValidCard(2);
-      gameState === 4 && setValidCard(1);
-    }
+    setTimeout(()=> {
+      if(seq === "1"){
+        gameState === 0 && setValidCard(6);
+        gameState === 1 && setValidCard(3);
+        gameState === 2 && setValidCard(4);
+        gameState === 3 && setValidCard(1);
+        gameState === 4 && setValidCard(5);
+      }
+      if(seq === "2"){
+        gameState === 0 && setValidCard(8);
+        gameState === 1 && setValidCard(5);
+        gameState === 2 && setValidCard(6);
+        gameState === 3 && setValidCard(4);
+        gameState === 4 && setValidCard(6);
+      }
+      if(seq === "3"){
+        gameState === 0 && setValidCard(5);
+        gameState === 1 && setValidCard(7);
+        gameState === 2 && setValidCard(2);
+        gameState === 3 && setValidCard(1);
+        gameState === 4 && setValidCard(8);
+      }
+      if(seq === "4"){
+        gameState === 0 && setValidCard(9);
+        gameState === 1 && setValidCard(4);
+        gameState === 2 && setValidCard(1);
+        gameState === 3 && setValidCard(3);
+        gameState === 4 && setValidCard(7);
+      }
+      if(seq === "5"){
+        gameState === 0 && setValidCard(3);
+        gameState === 1 && setValidCard(2);
+        gameState === 2 && setValidCard(6);
+        gameState === 3 && setValidCard(9);
+        gameState === 4 && setValidCard(1);
+      }
+      if(seq === "6"){
+        gameState === 0 && setValidCard(4);
+        gameState === 1 && setValidCard(9);
+        gameState === 2 && setValidCard(7);
+        gameState === 3 && setValidCard(8);
+        gameState === 4 && setValidCard(3);
+      }
+      if(seq === "7"){
+        gameState === 0 && setValidCard(5);
+        gameState === 1 && setValidCard(9);
+        gameState === 2 && setValidCard(7);
+        gameState === 3 && setValidCard(4);
+        gameState === 4 && setValidCard(8);
+      }
+      if(seq === "8"){
+        gameState === 0 && setValidCard(1);
+        gameState === 1 && setValidCard(2);
+        gameState === 2 && setValidCard(5);
+        gameState === 3 && setValidCard(9);
+        gameState === 4 && setValidCard(4);
+      }
+      if(seq === "9"){
+        gameState === 0 && setValidCard(6);
+        gameState === 1 && setValidCard(8);
+        gameState === 2 && setValidCard(7);
+        gameState === 3 && setValidCard(2);
+        gameState === 4 && setValidCard(1);
+      }
+    
+    }, "500");
+
+  //   if(seq === "1"){
+  //     gameState === 0 && setValidCard(6);
+  //     gameState === 1 && setValidCard(3);
+  //     gameState === 2 && setValidCard(4);
+  //     gameState === 3 && setValidCard(1);
+  //     gameState === 4 && setValidCard(5);
+  //   }
+  //   if(seq === "2"){
+  //     gameState === 0 && setValidCard(8);
+  //     gameState === 1 && setValidCard(5);
+  //     gameState === 2 && setValidCard(6);
+  //     gameState === 3 && setValidCard(4);
+  //     gameState === 4 && setValidCard(6);
+  //   }
+  //   if(seq === "3"){
+  //     gameState === 0 && setValidCard(5);
+  //     gameState === 1 && setValidCard(7);
+  //     gameState === 2 && setValidCard(2);
+  //     gameState === 3 && setValidCard(1);
+  //     gameState === 4 && setValidCard(8);
+  //   }
+  //   if(seq === "4"){
+  //     gameState === 0 && setValidCard(9);
+  //     gameState === 1 && setValidCard(4);
+  //     gameState === 2 && setValidCard(1);
+  //     gameState === 3 && setValidCard(3);
+  //     gameState === 4 && setValidCard(7);
+  //   }
+  //   if(seq === "5"){
+  //     gameState === 0 && setValidCard(3);
+  //     gameState === 1 && setValidCard(2);
+  //     gameState === 2 && setValidCard(6);
+  //     gameState === 3 && setValidCard(9);
+  //     gameState === 4 && setValidCard(1);
+  //   }
+  //   if(seq === "6"){
+  //     gameState === 0 && setValidCard(4);
+  //     gameState === 1 && setValidCard(9);
+  //     gameState === 2 && setValidCard(7);
+  //     gameState === 3 && setValidCard(8);
+  //     gameState === 4 && setValidCard(3);
+  //   }
+  //   if(seq === "7"){
+  //     gameState === 0 && setValidCard(5);
+  //     gameState === 1 && setValidCard(9);
+  //     gameState === 2 && setValidCard(7);
+  //     gameState === 3 && setValidCard(4);
+  //     gameState === 4 && setValidCard(8);
+  //   }
+  //   if(seq === "8"){
+  //     gameState === 0 && setValidCard(1);
+  //     gameState === 1 && setValidCard(2);
+  //     gameState === 2 && setValidCard(5);
+  //     gameState === 3 && setValidCard(9);
+  //     gameState === 4 && setValidCard(4);
+  //   }
+  //   if(seq === "9"){
+  //     gameState === 0 && setValidCard(6);
+  //     gameState === 1 && setValidCard(8);
+  //     gameState === 2 && setValidCard(7);
+  //     gameState === 3 && setValidCard(2);
+  //     gameState === 4 && setValidCard(1);
+  //   }
   }
 
   // console.log("hello");
